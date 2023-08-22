7be1f24d3ea0803a427816cfbf046551f1028278
mateo-brizuela <124217979+mateo-brizuela@users.noreply.github.com>
2023-08-22 15:47:43 -0300

    Add files via upload

---
+++ facultad/proyectos de c++(informatica 1 y 2)/informatica 2/repaso info 1/ejercicios/ejercicio_puntero 8.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+void c_vector(char *dab);
+void leer(char *dab);
+void p_mayuscula(char *dab);
+void p_minuscula(char *dab);
+
+int main(int argc, char *argv[]) {
+	char ab[26];
+	char *dab = ab;
+	char opcion = '0';
+	c_vector(dab);
+	
+	while(opcion != 's')
+	{
+		printf("seleccione una opcion:\n"
+			   "a) mostrar abecedario en mayusculas\n"
+			   "b) mostrar abecedario en minusculas\n"
+			   "s) salir\n");
+		scanf("%c",&opcion);
+		printf("\n");
+		
+		switch(opcion)
+		{
+		case 'a':
+				p_mayuscula(dab);
+				leer(dab);
+				break;
+				
+		case 'b':
+				p_minuscula(dab);
+				leer(dab);
+				break;	
+		}
+	}
+	
+	return 0;
+}
+
+void c_vector(char *dab) //cargar vector
+{
+	int i = 0;
+	for(i=0;i<26;i++)
+	{
+		*(dab+i) = i+65;
+	}
+}
+
+void leer(char *dab)
+{
+	int i = 0;
+	for(i=0;i<26;i++)
+	{
+		printf("%c",*(dab+i));
+	}
+	printf("\n\n");
+}
+
+void p_mayuscula(char *dab)
+{
+	int i = 0;
+	for(i=0;i<26;i++)
+	{
+		*(dab+i) = toupper(*(dab+i));
+	}
+}
+
+void p_minuscula(char *dab)
+{
+	int i = 0;
+	for(i=0;i<26;i++)
+	{
+		*(dab+i) = tolower(*(dab+i));
+	}
+}
+
+
---
+++ facultad/proyectos de c++(informatica 1 y 2)/informatica 2/repaso info 1/ejercicios/prueba_punterios.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+void contar(int v[5], int *dc1, int *dc2);
+	
+int main(int argc, char *argv[]) {
+	int v[5] = {1,2,1,1,2};
+	int c1 = 0;
+	int c2 = 0;
+	int *dc1 = nullptr;
+	int *dc2 = nullptr;
+	dc1 = &c1;
+	dc2 = &c2;
+	
+	contar(v, dc1, dc2);
+	printf("%d\n%d\n",c1,c2);
+	return 0;
+}
+
+void contar(int v[5], int *dc1, int *dc2)
+{
+	int i = 0;
+	for(i=0;i<5;i++)
+	{
+		if(v[i] == 1)
+		{
+			*dc1 = *dc1 +1;
+		}
+		
+		if(v[i] == 2)
+		{
+			*dc2 = *dc2 +1;
+		}
+	}
+}
+
