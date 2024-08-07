5c9643dbc85b49bcf6b921ee0ac3354bc34e022c
mateo-brizuela <124217979+mateo-brizuela@users.noreply.github.com>
2024-08-07 11:40:10 -0300

    Add files via upload

---
+++ proyectos de c++(informatica 1 y 2)/informatica 2/punteros/arreglo_punteros.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void leer(int *dv);
+
+int main(int argc, char *argv[]) {
+	int vector[3]= {1,2,3}; 
+	int *dv = vector;
+	leer(dv);
+	return 0;
+	
+	
+}
+
+void leer(int *dv)
+{
+	int i = 0;
+	for(i=0;i<3;i++)
+	{
+		printf("%d\t",*(dv+i));
+	}
+}
---
+++ proyectos de c++(informatica 1 y 2)/informatica 2/punteros/sumatoria_promedio_arreglo.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+void leer(float *dv);
+void csumatoria(float *dv,float *dsumatoria);
+void cpromedio(float *dsumatoria,float *dpromedio);
+
+int main(int argc, char *argv[]) {
+	float vector[5] = {4.5,8,2.3,1.1,3};
+	float *dv = vector; // direccion del vector
+	float sumatoria = 0;
+	float *dsumatoria = &sumatoria; //direccion de sumatoria
+	float  promedio = 0;
+	float *dpromedio = &promedio;
+	
+	csumatoria(dv,dsumatoria);
+	cpromedio(dsumatoria,dpromedio);
+	leer(dv);
+	printf("la sumatoria es: %.1f\n"
+		   "el promedio es: %.2f",sumatoria,promedio);
+	
+	
+	return 0;
+}
+
+void leer(float *dv)
+{
+	int i = 0;
+	printf("el vector es: ");
+	for(i=0;i<5;i++)
+	{
+		printf("%.1f\t",*(dv+i));
+	}
+	printf("\n");
+}
+
+void csumatoria(float *dv,float *dsumatoria) //calcular sumatoria5
+{
+	int i = 0;
+	for(i=0;i<5;i++)
+	{
+		*dsumatoria = (*dsumatoria) + (*(dv+i)); 
+	}
+}
+
+void cpromedio(float *dsumatoria,float *dpromedio) // calcular el promedio
+{
+	*dpromedio = (*dsumatoria)/5;
+}
+
